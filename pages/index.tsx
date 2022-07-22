742974772d140c2b2e5f2debd35f6daa0b0d57e5
Harsh2220 <harshgajjar145@gmail.com>
2022-07-23 02:10:31 +0530

    Signup and Login Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
 import { Center, Heading } from '@chakra-ui/react'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Login from '../components/Login'
+import Signup from '../components/SingUp'
 
 const Home: NextPage = () => {
   return (
-    <Center minH='100vh' bg='gray.800' color='white'>
-      <Heading fontSize='9xl'>Hello</Heading>
-    </Center>
+    <Login/>
   )
 }
 
