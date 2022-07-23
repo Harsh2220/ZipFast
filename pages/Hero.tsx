d384db4e8a83ffe12b950f551e6f6ee876adb796
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-07-23 18:18:18 +0530

    fea : script added for pull and npm i

--- pages/Hero.tsx
+++ pages/Hero.tsx
@@ -1,32 +1,14 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import '@fontsource/inter/700.css'
 import {
-  Box,
-  Flex,
-  Text,
-  Heading,
-  IconButton,
-  Image,
-  Button,
-  Stack,
-  Collapse,
-  Icon,
-  Link,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  useColorModeValue,
-  useBreakpointValue,
-  useDisclosure,
+  Heading, Stack
 } from '@chakra-ui/react';
+import '@fontsource/inter/700.css';
+import Navbar from '../components/Navbar';
 
 function Hero() {
   return (
     <div>
       <Navbar/>
       <Stack minH='87.9vh' textAlign={'center'} justifyContent={'center'}>
-        
         <Heading fontSize={'5xl'} mb='4' color={'#57a5bf'}>Why To Wait?</Heading>
         <Heading fontSize={'7xl'} bgGradient='linear(to-r, white, #6b61aa)' bgClip={'text'}>Just go ZipFast</Heading>
       </Stack>
