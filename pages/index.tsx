519b0435b1d1288951e8503c3557a11df78e07b9
sahilkakwani9 <sahkakwani@gmail.com>
2022-07-31 00:58:25 +0530

    add sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,21 @@ import type { NextPage } from 'next'
 import Dashboard from '../components/admin/Dashboard'
 import FoodCard from '../components/FoodCard'
 import Login from '../components/Login'
+import Sidebar from '../components/admin/Sidebar'
 import VerifyOtp from '../components/VerifyOtp'
 import useStore from '../store/Store'
 import Hero from "./Hero"
-import order from './order'
+// import order from './order'
 
 const Home: NextPage = () => {
 
   const state = useStore();
-  const otpsent = state.otpsent;
+  // const otpsent = state.otpsent;
   
   return (
     // (otpsent ? <VerifyOtp/> : <Login/> )
-    <FoodCard/>
+    <Sidebar/>
+    // <Hero/>
   )
 }
 
